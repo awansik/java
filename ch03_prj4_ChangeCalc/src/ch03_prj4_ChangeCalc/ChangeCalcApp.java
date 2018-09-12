85f10b63f03118047d78e0de0b4f16fab72406a4
Andrew Wansik <amwansik@gmail.com>
2018-09-12 16:16:32 -0400

    added ch 3 prj 4

---
+++ ch03_prj4_ChangeCalc/src/ch03_prj4_ChangeCalc/ChangeCalcApp.java
@@ -0,0 +1,15 @@
+package ch03_prj4_ChangeCalc;
+
+import java.util.Scanner;
+
+public class ChangeCalcApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Change Calculator");
+		Scanner sc = new Scanner (System.in);
+		 
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y"));
+	}
+
+}
