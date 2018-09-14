87b371f5ca06e2dc4dd677640da01877444764e3
Andrew Wansik <amwansik@gmail.com>
2018-09-14 09:06:02 -0400

    added ch 7 ex 3

---
+++ ch07_ex3_AreaAndPerimeter/src/Rectangle.java
@@ -0,0 +1,19 @@
+
+public class Rectangle {
+	private double length;
+	private double width;
+	
+	public Rectangle() {
+		length = 0;
+		width = 0;
+}
+
+	public Rectangle(double length, double width) {
+		this.length = length;
+		this.width = width;
+}
+	
+	public double getLength () {
+		return length;
+	}
+}
